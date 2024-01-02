4c4874e2589070c31fe2ef8e585f30f0ea09f236
mohammadrezagohari <eng.mr.gohari@gmail.com>
2024-01-02 14:30:44 +0330

    Initial commit of Ghasedak SMS package

---
+++ src/GhasedakSmsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MohammadrezaGohari\GhasedakSms;
+
+use Illuminate\Support\ServiceProvider;
+
+class GhasedakSmsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register the service the package provides.
+    }
+
+    public function boot()
+    {
+        // Perform any one-time boot operations here.
+    }
+}
